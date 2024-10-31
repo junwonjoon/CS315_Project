0f9ad369e00e9f15a67ee44bc20718901471bf03
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-10-31 15:29:15 -0700

    Beef up tests for graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,6 @@
-from node import FlightGraph, Flight
+from graph import FlightGraph, Flight
 import pytest
-from typing import List
+from typing import List, Set
 
 
 @pytest.fixture
@@ -23,11 +23,17 @@ def single_flight_string() -> str:
     )
     # fmt: on
 
+@pytest.fixture
+def example_flights():
+    return [
+        Flight("PHX", "LAS", (5.99)),
+        Flight("JFK", "MDW", (12.99)),
+        Flight("LAS", "MDW", (15.99)),
+        Flight("PHX", "JFK", (17.99)),
+        Flight("PHX", "MDW", (13.99))
+    ]
 
-def generate_string_from_flights(flights: List[Flight]) -> str:
-    """Generate the FlightGraph string from a list of flights"""
-    out = ""
-
+def generate_airports_from_flights(flights: List[Flight]) -> List[str]:
     airport_set = set()
 
     for f in flights:
@@ -37,11 +43,22 @@ def generate_string_from_flights(flights: List[Flight]) -> str:
     airports = list(airport_set)
     airports.sort()
 
+    return airports
+
+
+
+def generate_flightgraph_string(flights: List[Flight], airports: List[str]) -> str:
+    """Generate the FlightGraph string from a list of flights"""
+    out = ""
+
     out = "Airports: "
     out += ", ".join(airports) + "\n"
 
     for a in airports:
         outgoing_flighs = [f for f in flights if f.source == a]
+
+        outgoing_flighs.sort(key=lambda f: str(f))
+
         out += f"\nFlights from {a}:\n"
         for f in outgoing_flighs:
             out += str(f) + "\n"
@@ -51,14 +68,26 @@ def generate_string_from_flights(flights: List[Flight]) -> str:
 
 def test_helper_fn(single_flight, single_flight_string):
     """Verify that generate_string_from_flights behaves correctly"""
-    assert generate_string_from_flights([single_flight]) == single_flight_string
+    assert generate_flightgraph_string([single_flight], generate_airports_from_flights([single_flight])) == single_flight_string
 
 
 def test_update_1_flight(single_flight, single_flight_string):
-    """Test FlightGraph.update_flight function"""
+    """Test FlightGraph.update_flight function with a single flight"""
     flight_lst = FlightGraph(set([single_flight.source, single_flight.dest]))
     flight_lst.update_flight(single_flight)
     assert str(flight_lst) == single_flight_string
+
+
+def test_update_flight(example_flights):
+    """Test Flightgraph.update_flight function"""
+    airports = generate_airports_from_flights(example_flights)
+
+    flight_lst = FlightGraph(set(airports))
+    for f in example_flights:
+        flight_lst.update_flight(f)
+
+    print(flight_lst.airports)
+    assert str(flight_lst) == generate_flightgraph_string(example_flights, airports)
 
 
 def test_init():
