f77fcd79e00bbd0818df415a6aca2db1f0f29678
Wonjoon <junwonjoon41@gmail.com>
2024-11-08 13:03:34 -0700

    Updated main

--- funclib.py
+++ funclib.py
@@ -77,21 +77,6 @@ def is_in_circle(query_x: float, query_y: float, origin_x: float, origin_y: floa
     Returns:
         bool: True if the query point is within the radius, False otherwise.
     """
-def find_distance_and_midpoint(x1: float, y1: float, x2: float, y2: float) -> tuple[float | Any, float, float]:
-    distance_x = max(x1, x2) - min(x1, x2)
-    midpoint_x = (min(x1, x2) + distance_x / 2)
-    if distance_x >= 180:
-        distance_x = 360 - distance_x
-        midpoint_x = (min(x1, x2) - distance_x / 2)
-    distance_y = max(y1, y2) - min(y1, y2)
-    midpoint_y = min(y1, y2) + distance_y / 2
-    if distance_y >= 90:
-        distance_y = 180 - distance_y
-        midpoint_y = min(y1, y2) - distance_y / 2
-    return (distance_y ** 2 + distance_x ** 2) ** 0.5 / 2, midpoint_x, midpoint_y
-
-
-def is_in_circle(query_x, query_y, origin_x, origin_y, radius) -> bool:
     if find_distance_and_midpoint(query_x, query_y, origin_x, origin_y)[0] <= radius:
         return True
     else:
@@ -164,6 +149,7 @@ def get_valid_heading(departing_iata: str, arriving_iata: str, in_circle_df: pd.
                 graph.edge(outer,inner, label = str(round(price,2)))
                 list_of_valid_edges.append((outer, inner, round(price, 2)))
     return list_of_valid_edges, graph
+
 def getHTMLdocument(url):
     """
     from https://www.geeksforgeeks.org/beautifulsoup-scraping-link-from-html/
